0f7904d44704d260547b7e1929c0ef4dffdd8a08
Lucas <ukokuja@gmail.com>
2022-04-02 22:08:41 +0300

    Update predict path

--- controllers/analysis-controller.js
+++ controllers/analysis-controller.js
@@ -33,7 +33,7 @@ const getFullMedicalRecord = (patient, medical_record) => {
 }
 exports.analysisController = {
     getAnalysis(req, res) {
-        const predictPath = "http://localhost:8000/predict"
+        const predictPath = "http://3.250.76.68:8000/predict"
         console.log("search medical record id: ", req.body.medical_record_id);
         MedicalRecord.findOne({_id: req.body.medical_record_id}).then(medical_record => {
             console.log("found medical record for patient", medical_record.patient_id);
